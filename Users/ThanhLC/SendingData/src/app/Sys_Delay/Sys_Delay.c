0f9e16260f3ad7bff56e70ce97a204de2f08dcc7
ThanhLT17 <kevinthanh170613@gmail.com>
2018-07-27 15:07:23 +0700

    Ver1

---
+++ Users/ThanhLC/SendingData/src/app/Sys_Delay/Sys_Delay.c
@@ -0,0 +1,33 @@
+/******************************************************
+ *      Delay using Systick Timer
+ *
+ *****************************************************/
+#include "board.h"
+
+
+volatile uint32_t msTicks;
+
+static void Systick_Delay_Init()
+{
+    /* Activate SysTick */
+   SysTick_Config(SystemCoreClock/1000);
+
+   /* Update priority set by SysTick_Configuration */
+   NVIC_SetPriority(SysTick_IRQn, (1<<2) - 1);
+
+   /* Enable system tick interrupt */
+   NVIC_EnableIRQ(SysTick_IRQn);
+}
+
+
+void SysTick_Handler (void)
+{
+    msTicks ++;
+}
+
+void Delay_ms(uint32_t dlyTicks)
+{
+    uint32_t curTicks;
+    curTicks = msTicks;
+    while ((msTicks - curTicks) < dlyTicks);
+}
